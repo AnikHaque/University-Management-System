811e0bc7962ff9f13a97a57c96d7b5ac98327c99
AnikHaque <ekramul15-811@diu.edu.bd>
2026-02-02 00:16:25 +0600

    created department, teacher, student,teacher apps

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'departments',
+    'students',
+    'teachers',
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
